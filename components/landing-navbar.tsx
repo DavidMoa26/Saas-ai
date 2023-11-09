be14c780468eecfe5944f530a22168f8fcc7d32b
DavidMoa26 <117172647+DavidMoa26@users.noreply.github.com>
2023-11-09 13:42:14 +0200

    fix auth

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -12,18 +12,25 @@ import { useRouter } from "next/navigation";
 
 const font = Montserrat({ weight: '600', subsets: ['latin'] });
 
+
 export const LandingNavbar = () => {
+
+
+
 
   const { status } = useSession()
   const router = useRouter()
 
-  // const getStartedHandler = () => {
-  //   if (status === 'authenticated') {
-  //     router.push('/dashboard')
-  //   } else {
-  //     signIn('google', { callbackUrl: 'https://saas-ai-lilac.vercel.app/' })
-  //   }
-  // }
+  const getStartedHandler = () => {
+
+    const redirectUrlAfterLogin = process.env.BASE_URL + '/dashboard'
+
+    if (status === 'authenticated') {
+      router.push(redirectUrlAfterLogin)
+    } else {
+      signIn('google', { callbackUrl: redirectUrlAfterLogin })
+    }
+  }
 
   return (
     <nav className="p-4 bg-transparent flex items-center justify-between">
@@ -36,7 +43,7 @@ export const LandingNavbar = () => {
         </h1>
       </Link>
       <div className="flex items-center gap-x-2">
-        <Button onClick={() => signIn('google', { callbackUrl: 'https://saas-ai-lilac.vercel.app/' })} variant="outline" className="rounded-full">
+        <Button onClick={getStartedHandler} variant="outline" className="rounded-full">
           Get Started
         </Button>
       </div>
--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['oaidalleapiprodscus.blob.core.windows.net', 'soundhelix.com', 'i.ibb.co'],
   },
+  reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  }
 }
 
 module.exports = nextConfig
