c390ae4036a46c680b629194e38c3e2827375dfe
DavidMoa26 <117172647+DavidMoa26@users.noreply.github.com>
2023-11-12 16:06:59 +0200

    add middleware

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/navbar"
 import Sidebar from "@/components/sidebar"
 import { getApiLimit } from "@/lib/api-limit"
+import { getServerSession } from "next-auth";
 
 const DashboardLayout = async ({
   children
---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server';
+
+export default withAuth(
+  function middleware(req) {
+    // Get the pathname from the request URL
+    const pathname = new URL(req.url).pathname;
+
+    // If the user is not logged in and is trying to access a path other than the root
+    if (!req.nextauth.token && pathname !== '/') {
+      // Redirect them to the root path
+      return NextResponse.rewrite(new URL("/", req.url));
+    }
+
+    // Continue with the normal flow for other cases
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      // Authorize the user if they have a token or are requesting the root path
+      authorized: ({ token, req }) => {
+        const pathname = new URL(req.url).pathname;
+        return !!token || pathname === '/';
+      },
+    }
+  }
+);
+
+export const config = {
+  matcher: ["/:path*"], // Apply the middleware to all paths
+};
